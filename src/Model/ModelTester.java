cdd1b351655e6fa08867429f34cecc88aa384177
JiaJun Dai <jiajun.dai@sjsu.edu>
2020-12-03 11:50:53 -0800

    new Model classes

--- src/Model/ModelTester.java
+++ src/Model/ModelTester.java
@@ -1,29 +1,34 @@
-package Model; /**
- * author: JiaJun Dai
- * course: CS151
- * Description: This is just a demo, still working on it
- */
+package ModelTesting;
 import java.util.Scanner;
 
-public class PlayGame {
-
+public class ModelTester {
 	public static void main(String argv[]) {
 		Scanner s = new Scanner (System.in);
 		
-//=============== player class constructor will set all ships ========================
-		Player p1 = new Player();
+	//=============== player class constructor will set all ships ========================
+		PlayerModel p1 = new PlayerModel();
+		Ship s1 = new Ship(1,3,2,true);
+		Ship s2 = new Ship(4,0,3,true);
+		Ship s3 = new Ship(5,6,3,false);
+		Ship s4 = new Ship(7,1,4,true);
+		Ship s5 = new Ship(3,8,5,false);
+		p1.setShip(s1);
+		p1.setShip(s2);
+		p1.setShip(s3);
+		p1.setShip(s4);
+		p1.setShip(s5);
 		
-//================= AIPlayer class constructor will set all ships ==========================
-		AIPlayer p2 = new AIPlayer();
+			
+	//================= AIPlayer class constructor will set all ships ==========================
+		AIPlayerModel p2 = new AIPlayerModel();
 		
-		
-//================= This nested while loop takes control of hit & continue firing and game over ======================
+	//================= This nested while loop takes control of hit & continue firing and game over ======================
 		boolean playerContinueFire;
 		boolean AIContinueFire;
 		
 		while(true) {
 			while(true){
-				
+					
 				p1.display();
 				System.out.println("=====================================================");
 				p2.display();
@@ -38,15 +43,14 @@ public class PlayGame {
 				if(!playerContinueFire) {
 					break;
 				}
-				
-				if(p2.gameOver()){
+
+				if(p2.isGameOver()){
 					System.out.println("Congratulation! Player wins!!!!!");
 					return;
 				}
 			}
 			
 			while(true){
-				
 				p1.display();
 				System.out.println("=====================================================");
 				p2.display();
@@ -57,7 +61,7 @@ public class PlayGame {
 					break;
 				}
 				
-				if(p1.gameOver()) {
+				if(p1.isGameOver()) {
 					System.out.println("Unfortunately, AI wins!!!!!");
 					return;
 				}
