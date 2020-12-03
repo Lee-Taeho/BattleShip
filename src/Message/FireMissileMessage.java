dbfec6ad1c5007c8ca67cf765d27d779f412e337
JiaJun Dai <jdai10@mail.ccsf.edu>
2020-12-03 13:50:39 -0800

    updated FireMissileMessage

--- src/Message/FireMissileMessage.java
+++ src/Message/FireMissileMessage.java
@@ -4,4 +4,34 @@ public class FireMissileMessage {
 	int x;
 	int y;
 	boolean atPlayer;
+	
+	public FireMissileMessage() {
+		x = -1;
+		y = -1;
+		atPlayer = false;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public boolean isAI() {
+		return atPlayer;
+	}
+	
+	public void setIsAI(boolean isAI) {
+		this.atPlayer = atPlayer;
+	}
 }
