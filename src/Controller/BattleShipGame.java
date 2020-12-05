3743a0d46e697884463b9ffe7b036e7830742259
JiaJun Dai <jdai10@mail.ccsf.edu>
2020-12-04 19:27:44 -0800

    uploaded BattleShipGame main loop in Controller

---
+++ src/Controller/BattleShipGame.java
@@ -0,0 +1,24 @@
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class BattleShipGame {
+    private static BlockingQueue<Message> queue = new LinkedBlockingQueue<>();
+    private static View view;
+    private static PlayerModel p1;
+    private static AIPlayerModel p2;
+
+    public static void main(String[] args) {
+    	//creates View
+        view = View.init(queue);
+        //creates Model
+        p1 = new PlayerModel();
+        p2 = new AIPlayerModel();
+        Controller controller = new Controller(view, p1, p2, queue);
+
+        controller.mainLoop();
+        view.dispose();
+        queue.clear();
+    }
+}
+
