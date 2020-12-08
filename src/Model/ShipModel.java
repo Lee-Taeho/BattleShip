3b394dd73cd9efbba567ca67a45100857a83303f
JiaJun Dai <jdai10@mail.ccsf.edu>
2020-12-07 19:28:41 -0800

    Add files via upload

--- src/Model/ShipModel.java
+++ src/Model/ShipModel.java
@@ -1,39 +1,33 @@
 package Model;
-/** Represents a ship.
- * @author JiaJun Dai
-*/
+
 public class ShipModel {
 	private int xPos[];
 	private int yPos[];
 	private int length;
 	private int numHit;
 	private boolean vertical;
-	private boolean isHit[];
+	//private boolean isHit[];
 	
-	/**
-	* Constructor for AIPlayerModel
-	*/
+	//Ship constructor for AIPlayerModel
 	public ShipModel(int length) {
 		this.length = length;
 		xPos = new int[length];
 		yPos = new int[length];
-		isHit = new boolean[length];
+		//isHit = new boolean[length];
 	}
 	
-	/**
-	* Constructor for playerModel
-	*/
+	//Ship constructor for PlayerModel
 	public ShipModel(int x, int y, int length, boolean vertical) {
 		this.length = length;
 		xPos = new int[length];
 		yPos = new int[length];
-		isHit = new boolean[length];
+		//isHit = new boolean[length];
 		this.vertical = vertical;
 		
 		for (int i = 0; i < length; i++) {
 			xPos[i] = x;
 			yPos[i] = y;
-			isHit[i] = false;
+			//isHit[i] = false;
 			if(vertical) {
 				y++;
 			}else {
@@ -44,20 +38,13 @@ public class ShipModel {
 	}
 	
 	/**
-	* Register the hit into a boolean array isHit[]
-	* @param x x position of the hit
-	* @param y y position of the hit
-	* @return true if 
-	*/
-	public boolean registerHit(int x, int y) {
+	public void registerHit(int x, int y) {
 		for(int i = 0; i < length; i++) {
 			if(xPos[i] == x && yPos[i] == y && isHit[i] == false) {
 				numHit++;
 				isHit[i] = true;
-				return true;
 			}
 		}
-		return false;
 	}
 	
 	public boolean checkIfSunk() {
@@ -67,6 +54,7 @@ public class ShipModel {
 			return false;
 		}
 	}
+	*/
 	
 	public int getGridDisplay() {
 		if(length == 2) {
@@ -105,10 +93,6 @@ public class ShipModel {
 	
 	public void setYPos(int[] yPos) {
 		this.yPos = yPos;
-	}
-	
-	public boolean[] getIsHit() {
-		return isHit;
 	}
 	
 	public boolean isVertical() {
