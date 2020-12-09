62bdbaa5cba98892c985eae0fdfcf799acb3cd8d
JiaJun Dai <jdai10@mail.ccsf.edu>
2020-12-09 13:30:41 -0800

    Junit testing classes in a package

---
+++ src/JunitTesting/AIPlayerModelTest.java
@@ -0,0 +1,30 @@
+package JunitTesting;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import Model.AIPlayerModel;
+
+class AIPlayerModelTest {
+
+	AIPlayerModel p2 = new AIPlayerModel();
+	
+	
+	@Test
+	void testAttackedByPlayer() {
+		assertFalse(p2.attackedByPlayer(2, 2));
+	}
+	
+	@Test
+	void testIsGameOver() {
+		p2.attackedByPlayer(2,2);
+		assertFalse(p2.isGameOver());
+	}
+	
+	@Test
+	void testReset(){
+		p2.reset();
+		assertEquals(0, p2.getNumOfHits());
+	}
+}
